56b7a0d15a59ce5c0cb1eae0c6f912ebf59da52b
Abhilov23 <abhilovgupta01@gmail.com>
2025-01-03 17:18:16 +0530

    new update

--- rust-programs/src/bin/8_struct.rs
+++ rust-programs/src/bin/8_struct.rs
@@ -1,23 +1,56 @@
-struct User{
-    username: String,
-    email: String,
-    sign_in_count: u64,
-    active: bool,
+//lets discuss about all the ways of creating a struct
+
+struct Point {
+    x: f32,
+    y: f32,
 }
 
-fn main(){
-    let user1: User = User{
-        username: String::from("Abhilov"),
-        email: String::from("abhilovgupta01@gmail.com"),
-        sign_in_count: 56,
-        active: true,
-    };
-    println!("{}", user1.username);
+// Tuple
+struct Point3d(i32, i32, i32);
 
-    let user2: User = User{
-    username: String::from("John Doe"),
-    email: String::from("johndoe@gmail.com"),
-    sign_in_count: 100,
-    active: false,
+// Empty
+struct Empty;
+
+// Nested
+#[derive(Debug)]
+struct Circle {
+    center: Point,
+    radius: u32,
+}
+
+fn main() {
+    // Create
+    let p = Point { x: 1.0, y: 1.0 };
+    println!("p.x = {}", p.x);
+    println!("p.y = {}", p.y);
+    // Debug
+    println!("{:?}", p);
+
+    let p = Point3d(1, 2, 3);
+    println!("{} {} {}", p.0, p.1, p.2);
+
+    let empty = Empty;
+
+    let circle = Circle {
+        center: Point { x: 0.0, y: 0.0 },
+        radius: 3,
     };
+    println!("{:?}", circle);
+
+    // Shortcut
+    let x = 1.0;
+    let y = 1.0;
+    let p = Point { x, y };
+
+    // Copy fields
+    let p0 = Point { x: 1.0, y: 2.0 };
+    let p1 = Point { x: 2.0, ..p0 };
+
+    println!("{:?}", p1);
+
+    // Update
+    let mut p = Point { x: 0.0, y: 0.0 };
+    p.x += 1.0;
+    p.y += 1.0;
+    println!("{:?}", p);
 }
